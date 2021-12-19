65140dde56ecbe4ea66e7ed70b3b72dafe5034dd
Charlie Jonas <charlie@charliejonas.co.uk>
2021-12-19 14:53:15 +0000

    Avoid header being quenched by reverse proxy

--- middleware.go
+++ middleware.go
@@ -42,7 +42,7 @@ func loggingMiddleware(next http.Handler) http.Handler {
 
 func serverHeaderMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Server", "https://github.com/CHTJonas/ddnsd")
+		w.Header().Set("X-Powered-By", "https://github.com/CHTJonas/ddnsd")
 		next.ServeHTTP(w, r)
 	})
 }
